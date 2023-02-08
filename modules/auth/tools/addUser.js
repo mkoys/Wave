398b0feaa966d0aac0dd3e073707960ffee6f2c3
mkoys <mkoys@protonmail.com>
2023-02-08 18:16:45 +0100

    RegisterUser, register data check

--- modules/auth/tools/addUser.js
+++ modules/auth/tools/addUser.js
@@ -7,10 +7,10 @@ import processData from "../library/processData.js";
 export default async (data) => {
     const registerData = await read("data", { name: "register" });
 
-    if (registerData === false) { { error: "No register data were found" } }
+    if (registerData === false) { return { error: "No register data were found" } }
 
-    const checkErrors = await processData(registerData.rules, data);
-    if (checkErrors.errors.length) { return checkErrors }
+    const dataErrors = await processData(registerData.rules, data);
+    if (dataErrors.errors.length) { return checkErrors }
 
     const processErrors = await processData(registerData.process, data);
     if (processErrors.errors.length) { return processErrors }
