295744e222e76f97f7e774bc23641b3f98bb80a9
mkoys <mkoys@protonmail.com>
2023-02-28 11:00:45 +0100

    Repa rebase

--- modules/repa/exports.js
+++
@@ -1,6 +0,0 @@
-import router from "./router.js";
-
-export default {
-    router,
-}
-
--- modules/repa/tools/addAttenadnce.js
+++
@@ -1,9 +0,0 @@
-import wave from "../../../wave.js";
-
-export default async (data) => {
-   const filter = {date: data.date};
-   await wave.addon.remove("repa", filter);
-   if(data.content.length != 0) {
-      await wave.addon.insert("repa", data);
-   }
-}
